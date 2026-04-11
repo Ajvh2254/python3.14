88378f7c1dbb483b556cbbbcd46faf03dc7eae1e
cuzi <cuzixsin@gmail.com>
2026-04-10 20:06:31 -0700

    iterate through loops using generators

---
+++ src/main/challenges/prime_number_generators.py
@@ -0,0 +1,15 @@
+class PrimeGenerator:
+    def __init__(self, stop):
+        self.stop = stop
+        self.start = 2
+
+    def __next__(self):
+        for n in range(self.start, self.stop):
+            for x in range(2, n):
+                if n % x == 0:
+                    break
+                else:
+                    self.start = n + 1
+                    return n
+            raise StopIteration()
+        return None
